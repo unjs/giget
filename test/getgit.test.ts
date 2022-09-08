7ceba56fbfe465df24730eb4a3ff6a4992614efb
Pooya Parsa <pooya@pi0.io>
2022-09-08 17:44:22 +0200

    test: update fixture to create file in test dir

--- test/getgit.test.ts
+++ test/getgit.test.ts
@@ -1,5 +1,5 @@
 import { existsSync } from 'fs'
-import { rm, mkdir } from 'fs/promises'
+import { rm, mkdir, writeFile } from 'fs/promises'
 import { expect, it, describe, beforeAll } from 'vitest'
 import { resolve } from 'pathe'
 import { downloadRepo } from '../src'
@@ -18,6 +18,7 @@ describe('downloadRepo', () => {
   it('do not clone to exisiting dir', async () => {
     const dstDir = resolve(__dirname, '.tmp/exisiting')
     await mkdir(dstDir).catch(() => {})
+    await writeFile(resolve(dstDir, 'test.txt'), 'test')
     await expect(downloadRepo('unjs/template', dstDir)).rejects.toThrow('already exists')
   })
 })
