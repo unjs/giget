da327b5a602fa542360b0d2b7588d520c4177dff
Pooya Parsa <pooya@pi0.io>
2022-09-09 14:26:30 +0200

    fix: use github cdn for now

--- src/registry.ts
+++ src/registry.ts
@@ -1,11 +1,12 @@
 import fetch from 'node-fetch-native'
 import type { TemplateInfo, TemplateProvider } from './types'
 
-const DEFAULT_REGISTRY = 'https://cdn.jsdelivr.net/gh/unjs/giget/templates'
+// const DEFAULT_REGISTRY = 'https://cdn.jsdelivr.net/gh/unjs/giget/templates'
+const DEFAULT_REGISTRY = 'https://raw.githubusercontent.com/unjs/giget/main/templates'
 
 export const createRegistryProvider = (registryEndpoint: string = DEFAULT_REGISTRY) => {
   return <TemplateProvider>(async (input) => {
-    const registryURL = `${registryEndpoint}/${input}`
+    const registryURL = `${registryEndpoint}/${input}.json`
     const res = await fetch(registryURL)
     if (res.status >= 400) {
       throw new Error(`Failed to download ${input} template info from ${registryURL}: ${res.status} ${res.statusText}`)
