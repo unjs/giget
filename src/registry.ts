7984e08c6804f4eb47e6ce7298d782273fafb08a
Pooya Parsa <pooya@pi0.io>
2022-09-10 14:11:12 +0200

    chore: add debug for registry lookup time

--- src/registry.ts
+++ src/registry.ts
@@ -1,11 +1,13 @@
 import fetch from 'node-fetch-native'
 import type { TemplateInfo, TemplateProvider } from './types'
+import { debug } from './_utils'
 
 // const DEFAULT_REGISTRY = 'https://cdn.jsdelivr.net/gh/unjs/giget/templates'
 const DEFAULT_REGISTRY = 'https://raw.githubusercontent.com/unjs/giget/main/templates'
 
 export const registryProvider = (registryEndpoint: string = DEFAULT_REGISTRY) => {
   return <TemplateProvider>(async (input) => {
+    const start = Date.now()
     const registryURL = `${registryEndpoint}/${input}.json`
     const res = await fetch(registryURL)
     if (res.status >= 400) {
@@ -15,6 +17,7 @@ export const registryProvider = (registryEndpoint: string = DEFAULT_REGISTRY) =>
     if (!info.tar || !info.name) {
       throw new Error(`Invalid template info from ${registryURL}. name or tar fields are missing!`)
     }
+    debug(`Fetched ${input} template info from ${registryURL} in ${Date.now() - start}ms`)
     return info
   })
 }
