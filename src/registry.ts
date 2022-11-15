e579a2f5347356f3abed42459a90199adcc2df52
ikexing <ikexing@qq.com>
2022-11-15 09:49:55 +0800

    feat: support http proxy (#24)
    * feat: support http proxy
    
    * chore:  not need ts-ignore
    
    * fix: do not create proxy when it is false

--- src/registry.ts
+++ src/registry.ts
@@ -1,6 +1,5 @@
-import fetch from 'node-fetch-native'
 import type { TemplateInfo, TemplateProvider } from './types'
-import { debug } from './_utils'
+import { debug, sendFetch } from './_utils'
 
 // const DEFAULT_REGISTRY = 'https://cdn.jsdelivr.net/gh/unjs/giget/templates'
 const DEFAULT_REGISTRY = 'https://raw.githubusercontent.com/unjs/giget/main/templates'
@@ -9,7 +8,8 @@ export const registryProvider = (registryEndpoint: string = DEFAULT_REGISTRY) =>
   return <TemplateProvider>(async (input) => {
     const start = Date.now()
     const registryURL = `${registryEndpoint}/${input}.json`
-    const res = await fetch(registryURL)
+
+    const res = await sendFetch(registryURL)
     if (res.status >= 400) {
       throw new Error(`Failed to download ${input} template info from ${registryURL}: ${res.status} ${res.statusText}`)
     }
