a751edf40889731966f3867904423ea053be0f8a
Nick Petalas <webmasternikos@gmail.com>
2023-08-15 15:56:42 +0100

    fix: adjusting regex to allow / in git refs

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -6,6 +6,8 @@ describe("parseGitURI", () => {
   const tests = [
     { input: "org/repo", output: {} },
     { input: "org/repo#ref", output: { ref: "ref" } },
+    { input: "org/repo#ref-123", output: { ref: "ref-123" } },
+    { input: "org/repo#ref/ABC-123", output: { ref: "ref/ABC-123" } },
     { input: "org/repo/foo/bar", output: { subdir: "/foo/bar" } },
   ];
 
