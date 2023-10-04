58757a4b5c2dccf13fdf0e87308a3e0fbd292589
Pooya Parsa <pooya@pi0.io>
2023-10-04 21:04:31 +0200

    small refactors

--- src/providers.ts
+++ src/providers.ts
@@ -3,16 +3,22 @@ import { parseGitURI } from "./_utils";
 
 export const github: TemplateProvider = (input, options) => {
   const parsed = parseGitURI(input);
-  const github = process.env.GIGET_GITHUB_URL || "https://api.github.com";
+
+  // https://docs.github.com/en/rest/repos/contents#download-a-repository-archive-tar
+  // TODO: Verify solution for github enterprise
+  const githubAPIURL = process.env.GIGET_GITHUB_URL || "https://api.github.com";
+
   return {
     name: parsed.repo.replace("/", "-"),
     version: parsed.ref,
     subdir: parsed.subdir,
     headers: {
-      authorization: options.auth ? `token ${options.auth}` : undefined,
+      Authorization: options.auth ? `Bearer ${options.auth}` : undefined,
+      Accept: 'application/vnd.github+json',
+      'X-GitHub-Api-Version': '2022-11-28'
     },
-    url: `${github}/${parsed.repo}/tree/${parsed.ref}${parsed.subdir}`,
-    tar: `${github}/repos/${parsed.repo}/tarball/${parsed.ref}`,
+    url: `${githubAPIURL.replace('api.github.com', 'github.com')}/${parsed.repo}/tree/${parsed.ref}${parsed.subdir}`,
+    tar: `${githubAPIURL}/repos/${parsed.repo}/tarball/${parsed.ref}`,
   };
 };
 
