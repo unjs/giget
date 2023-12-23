a95f9d8901634b9cb0d86ea62f04cae16c285028
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-23 16:20:02 -0500

    refactor: more concise syntax
    Co-authored-by: Pooya Parsa <pyapar@gmail.com>

--- src/cli.ts
+++ src/cli.ts
@@ -74,7 +74,7 @@ const mainCommand = defineCommand({
     });
 
     const _from = r.name || r.url;
-    const _to = process.cwd() === r.dir ? "./" : relative(process.cwd(), r.dir);
+    const _to = relative(process.cwd(), r.dir) || "./";
     consola.log(`✨ Successfully cloned \`${_from}\` to \`${_to}\`\n`);
 
     if (args.shell) {
