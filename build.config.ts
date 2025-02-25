946ea3f6d3cb52223527612618f261b8f7378cea
Pooya Parsa <pooya@pi0.io>
2025-02-25 21:07:06 +0100

    build: minify bundled tar dependencies

--- build.config.ts
+++ build.config.ts
@@ -1,20 +1,34 @@
 import { defineBuildConfig } from "unbuild";
 import { rm } from "node:fs/promises";
+import { transform } from "esbuild";
+
+const tarDeps = [
+  "tar",
+  "fs-minipass",
+  "minipass",
+  "minizlib",
+  "yallist",
+  "mkdirp",
+  "chownr",
+];
 
 export default defineBuildConfig({
   rollup: {
-    inlineDependencies: [
-      // tar v6 dependencies
-      "tar",
-      "fs-minipass",
-      "minipass",
-      "minizlib",
-      "yallist",
-      "mkdirp",
-      "chownr",
-    ],
+    inlineDependencies: [...tarDeps],
   },
+
   hooks: {
+    "rollup:options"(ctx, opts) {
+      opts.plugins.push({
+        name: "selective-minify",
+        async transform(code, id) {
+          if (tarDeps.some((dep) => id.includes(`node_modules/${dep}/`))) {
+            const res = await transform(code, { minify: true });
+            return res.code;
+          }
+        },
+      });
+    },
     async "build:done"() {
       await rm("dist/index.d.ts");
       await rm("dist/cli.d.ts");
