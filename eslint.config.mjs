376e8be0c0b914cb86fd9fc5fbf76914f3a6d204
Ted Kesgar <t.kesgar@outlook.com>
2025-04-19 05:54:39 +0700

    eslint should ignore test/.tmp

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import unjs from "eslint-config-unjs";
 
 // https://github.com/unjs/eslint-config
 export default unjs({
-  ignores: [],
+  ignores: ["test/.tmp"],
   rules: {
   "unicorn/prefer-module": 0
 },
