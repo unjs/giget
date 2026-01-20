407f55a91757f5275724f7d02b0ca89acdcffa23
Pooya Parsa <pooya@pi0.io>
2026-01-20 13:58:40 +0100

    build: min libs

--- build.config.ts
+++ build.config.ts
@@ -1,5 +1,20 @@
 import { defineBuildConfig } from "obuild/config";
+import { minifySync } from "rolldown/experimental";
+import type { Plugin } from "rolldown";
 
 export default defineBuildConfig({
   entries: [{ type: "bundle", input: ["src/index.ts", "src/cli.ts"] }],
+  hooks: {
+    rolldownConfig: (config) => {
+      config.plugins ??= [];
+      (config.plugins as Plugin[]).push({
+        name: "min-libs",
+        renderChunk(code, chunk) {
+          if (chunk.fileName.startsWith("_chunks/libs/")) {
+            return minifySync(chunk.fileName, code, {});
+          }
+        },
+      });
+    },
+  },
 });
