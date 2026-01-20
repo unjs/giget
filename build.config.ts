fe78c04b927af72074dee31af8f889de13608062
Pooya Parsa <pooya@pi0.io>
2026-01-20 10:39:14 +0100

    build: migrate to obuild / rolldown

--- build.config.ts
+++ build.config.ts
@@ -1,38 +1,5 @@
-import { defineBuildConfig } from "unbuild";
-import { rm } from "node:fs/promises";
-import { transform } from "esbuild";
-
-const tarDeps = [
-  "tar",
-  "fs-minipass",
-  "minipass",
-  "minizlib",
-  "yallist",
-  "mkdirp",
-  "chownr",
-];
+import { defineBuildConfig } from "obuild/config";
 
 export default defineBuildConfig({
-  rollup: {
-    inlineDependencies: [...tarDeps],
-  },
-
-  hooks: {
-    "rollup:options"(ctx, opts) {
-      opts.plugins.push({
-        name: "selective-minify",
-        async transform(code, id) {
-          if (tarDeps.some((dep) => id.includes(`node_modules/${dep}/`))) {
-            const res = await transform(code, { minify: true });
-            return res.code;
-          }
-        },
-      });
-    },
-    async "build:done"() {
-      await rm("dist/index.d.ts");
-      await rm("dist/cli.d.ts");
-      await rm("dist/cli.d.mts");
-    },
-  },
+  entries: [{ type: "bundle", input: ["src/index.ts", "src/cli.ts"] }],
 });
